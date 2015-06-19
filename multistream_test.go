08e8f9c9f5665ed0c63ffde4fa5ef1d5fb3d516d
Jeromy <jeromyj@gmail.com>
2015-06-19 10:44:09 -0700

    improve tests

--- multistream_test.go
+++ multistream_test.go
@@ -1,6 +1,8 @@
 package multistream
 
 import (
+	"crypto/rand"
+	"io"
 	"net"
 	"testing"
 	"time"
@@ -36,6 +38,8 @@ func TestProtocolNegotiation(t *testing.T) {
 		t.Fatal("protocol negotiation didnt complete")
 	case <-done:
 	}
+
+	verifyPipe(t, a, b)
 }
 
 func TestSelectOne(t *testing.T) {
@@ -71,5 +75,79 @@ func TestSelectOne(t *testing.T) {
 	case <-time.After(time.Second):
 		t.Fatal("protocol negotiation didnt complete")
 	case <-done:
+	}
+
+	verifyPipe(t, a, b)
+}
+
+func TestSelectOneAndWrite(t *testing.T) {
+	a, b := net.Pipe()
+
+	mux := NewMultistreamMuxer()
+	mux.AddHandler("/a", nil)
+	mux.AddHandler("/b", nil)
+	mux.AddHandler("/c", nil)
+
+	done := make(chan struct{})
+	go func() {
+		selected, _, err := mux.Negotiate(a)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if selected != "/c" {
+			t.Fatal("incorrect protocol selected")
+		}
+		close(done)
+	}()
+
+	sel, err := SelectOneOf([]string{"/d", "/e", "/c"}, b)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if sel != "/c" {
+		t.Fatal("selected wrong protocol")
+	}
+
+	select {
+	case <-time.After(time.Second):
+		t.Fatal("protocol negotiation didnt complete")
+	case <-done:
+	}
+
+	verifyPipe(t, a, b)
+}
+
+func verifyPipe(t *testing.T, a, b io.ReadWriter) {
+	mes := make([]byte, 1024)
+	rand.Read(mes)
+	go func() {
+		b.Write(mes)
+		a.Write(mes)
+	}()
+
+	buf := make([]byte, len(mes))
+	n, err := a.Read(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n != len(buf) {
+		t.Fatal("failed to read enough")
+	}
+
+	if string(buf) != string(mes) {
+		t.Fatal("somehow read wrong message")
+	}
+
+	n, err = b.Read(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n != len(buf) {
+		t.Fatal("failed to read enough")
+	}
+
+	if string(buf) != string(mes) {
+		t.Fatal("somehow read wrong message")
 	}
 }
